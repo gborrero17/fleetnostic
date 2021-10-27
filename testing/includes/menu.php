4f97dc3371e19fc6319369abc3352a51a2f082fb
sean <sean@devusol.com>
2021-10-27 15:45:38 -0400

    download Nancy FTP 10_27_21_15:45

--- testing/includes/menu.php
+++ testing/includes/menu.php
@@ -1,4 +1,98 @@
+<div id="navborder">
+   <div id="navigation-menu">
 
-<div id="">
-          
+ 
+    
+ 
+  <nav class="navbar navbar-expand-custom">
+
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navmenu" aria-controls="navmenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+        <svg viewBox="0 0 100 80" width="35" height="40">
+          <rect width="100" height="10" rx=10></rect>
+          <rect y="30" width="100" height="10" rx=10></rect>
+          <rect y="60" width="100" height="10" rx=10></rect>
+        </svg>
+      </span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navmenu">
+ 
+       
+          <ul class="navbar-nav nav-fill mr-auto w-100">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">ABOUT US</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a id="solutions-dropdown" img-src="images/BM3010_PP0001.jpg" href="pbbt-solutions.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">PBBT SOLUTIONS</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="air-system-expert.php">AIR SYSTEM EXPERT</a>
+            </li>
+
+            <li class="nav-item dropdown text-decoration-none" data-toggle="collapse" data-target="#navbar-collapse">
+              <a id="served-dropdown" img-src="images/PBBT_011.jpg" href="industries-served.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">INDUSTRIES SERVED</a>
+            </li>
+
+            <li class="nav-item ">
+              <a class="nav-link" href="contact.php">CONTACT US</a>
+            </li>
+          </ul>
+    
+
+
+    </div>
+  </nav>
+  
   </div>
+
+  <div class="dropdown large-dropdown">
+
+    <ul class="col-md-offset-2 col-md-10 dropdown-menu solutions-menu">
+      <li class="image"><img src="images/BM3010_PP0001.jpg" /></li>
+      <li style="margin-top: 15px" class="dropdown ">
+        <hr class="line" />
+        <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-14200.php">14200</a>
+      </li>
+
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-20200.php">20200</a>
+      </li>
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-3010.php">3010</a>
+      </li>
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="play-detector.php">PLAY DETECTOR</a>
+      </li>
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="lease-audit.php">LEASE/AUDIT</a>
+      </li>
+    </ul>
+
+    <ul class="col-md-offset-2 col-md-10 dropdown-menu served-menu">
+      <li class="image"><img src="images/BM3010_PP0001.jpg" /></li>
+      <li style="margin-top: 15px" class="dropdown">
+        <hr class="line" />
+        <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="enforcement-roadside.php">Enforcement/Roadside</a>
+      </li>
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="fleet-transit.php">Fleet/Transit</a>
+      </li>
+      <li class="dropdown">
+        <hr class="line" />
+        <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="end-of-line.php">End-Of-Line</a>
+      </li>
+
+    </ul>
+
+  </div>
+</div>
--- testing/index-1.php
+++ testing/index-1.php
@@ -13,7 +13,7 @@
   src="//code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>	
-<?php include 'includes/includes.php'; ?>          
+<?php include 'includes/includes1022.php'; ?>          
   <style>
   /* Make the image fully responsive */
   .carousel-inner img {
@@ -39,11 +39,7 @@
 <div id="section2">
      <div id="section2Content">   
     <div class="smallCol under767">
-        <div class="img-wrapper text-center"><img src="images/pics/intropic.jpg" class="img-fluid paddingRight40" alt="">
-         <div class="img-overlay">
-                <a href="about.php">
-                <button class="buttonHome">About Fleetnostic</button>
-        </a>             </div></div>
+        <div class="text-center"><img src="images/pics/intropic.jpg" class="img-fluid paddingRight40" alt=""></div>
     </div>
          
     <div class="wideCol">
@@ -54,12 +50,7 @@
 Additionally, these systems can be used for roadside enforcement of brake systems and end-of-line testing in a manufacturing setting.</p>
          </div>
   <div class="smallCol marginRight30 over767">
-        <div class="img-wrapper text-center"><img src="images/pics/intropic.jpg" class="img-fluid paddingRight40" alt="">
-         <div class="img-overlay">
-               <a href="about.php">
-                <button class="buttonHome">About Fleetnostic</button>
-        </a>
-             </div></div>
+        <div class="text-center"><img src="images/pics/intropic.jpg" class="img-fluid" alt="The Tulip Festival Photography Contest"></div>
     </div>        
           
          </div></div>    
@@ -115,48 +106,14 @@ Additionally, these systems can be used for roadside enforcement of brake system
     </div>
     
 <div class="container-fluid" id="section4">	 
-   <div class="container" id="section4Content">  
-     <div class="row noPadMarg max1600">
-    <div class="img-wrapper smallCol">
-        <div  id="sect4ABkgd"> 
-      <p>Our testers fully comply with the US DOT FMCSA-98-3611 (FR 48799), “Functional Specifications for Performance- based brake testers (PBBT) for commercial motor vehicles.” With large rollers, bearings, and drive systems, they are designed to offer years of reliable service. </p>
-          <div class="img-overlay"><a href="pbbt-solutions.php">
-                <button class="buttonHome">PBBT Solutions</button>
-              </a> </div> </div>
-       
+   <div class="container" id="section4Content">   
+    <div class="twoCol">
+      <p>text goes here</p>
     </div>
          
-    <div class="wideCol">
-        <div id="sect4BBkgd">
-        <h2 class="font42">NewsLINK Sign-up</h2>
-        <p><strong>Subscribe to NewsLINK, our digital newsletter to receive the latest updates from LINK! The newsletter will keep you up to date on company news and events, product advancements, and more.</strong> </p>
-            <div>
-              <form> 
-                  <div class="formThreeCol">
-                  <div class="form-group threeColForm">
-                  <label for="First Name">First Name</label>
-                  <input type="text" class="form-control" id="fleetFirst" placeholder="">
-                  </div>
-                  <div class="form-group threeColForm">
-                  <label for="Last Name">Last Name</label>
-                  <input type="text" class="form-control" id="fleetLast" placeholder="">
-                  </div>
-                <div class="form-group threeColForm">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="fleetEmail" placeholder="">
-                  </div>
-                <div class="form-group oneCol">
-                  <label for="Industry">Industry</label>
-                  <input type="text" class="form-control" id="fleetIndustry" placeholder="">
-                </div></div>
-                  <div id="formSubmit">
-                      <div class="fiveCol">
-                  <button id="fleetSubmit" type="submit" class="btn btn-primary">Submit</button></div></div>
-              </form>
-            </div>
-            <p class="paddingTop30">LINK needs the contact information you provide to us to contact you about our products and services. You may unsubscribe from these communications at any time. For information on how to unsubscribe, as well as our privacy practices and commitment to protecting your privacy, please review our Privacy Policy. </p>
-        </div>
-         </div></div></div></div>
+    <div class="twoCol">
+        <h2 class="font42">Fleetnostic</h2>
+         </div></div></div>
     
 </div>   
 <?php include 'includes/footer.php'; ?> 

d215822faf81ad286ed89c991be6b1b28e0831d1
sean <sean@devusol.com>
2021-10-27 17:43:22 -0400

    added devusol folder to track our changes only

---
+++ devusol/testing/includes/head.php
@@ -0,0 +1,19 @@
+<div class="mx-auto d-block" id="header">
+ 
+<div class="mx-auto d-block" id="headerContent">
+    
+    
+  <div id="fleetHead">
+      
+    <div class="leftCenter767"><a href="index.php"><img src="images/head-foot/fleetnostic-foot.png" id="fleetLogo" class="img-fluid" alt="Fleetnostic logo"></a></div>
+    
+      
+	
+        <div class="paddingTop10">
+    <div class="rightCenter767">
+        <span class="font18">Email Fleetnostic&nbsp;&nbsp;&nbsp;</span><span class="font18" style="white-space: nowrap">       (555) 555-5555</span>
+    </div>
+  </div>
+</div>   
+  </div>  
+</div>
---
+++ devusol/testing/industries-served.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+    <!-- Google analytics goes here-->
+    <title>Fleetnostic</title>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/> 
+             
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+<script
+  src="//code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>	
+<?php include 'includes/includes.php'; ?>   
+</head>
+
+<body>
+    <!-- Head -->
+ <div id="sticky" class="sticky-top">
+<?php include 'includes/head.php'; ?>     
+<?php include 'includes/menu.php'; ?>
+  </div>
+<div id="main">  
+    
+    <!-- Hero -->
+   <div id="pbbtSolHero" class="ohioHero">	 
+  <h1 class="text-center"><span class="font50caps">Industries Served</span><br></h1>
+    </div>  
+    
+    <!-- pbbt section 1 -->
+    <div class="landingHeading1">
+        <div class="landingContent1">
+            
+            <div class="lH1 greenStrp1">&nbsp;</div>
+       
+        <div class="lH2 font42 text-left blue1">
+            <h2 class="landingCopy1">Insert Heading Here</h2>
+        </div>
+            
+        </div>
+        </div> 
+    
+    <!-- pbbt section 2 -->
+    <div class="basicTextSect mx-auto d-block">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis neque commodo purus auctor faucibus et eu risus. Integer fermentum mattis massa, et aliquam orci convallis ac. Nunc interdum urna leo, eget imperdiet sem volutpat ut. Ut nec semper mauris. Maecenas accumsan pharetra ligula sed interdum. Aliquam id quam laoreet, malesuada lectus eget, mattis turpis. Aenean non magna quis ligula pulvinar faucibus sed vel nulla. Nulla placerat eu tortor at placerat. Fusce porttitor varius mollis.</p>
+<p>Integer scelerisque ut mi et blandit. Maecenas consectetur aliquet ipsum id rhoncus. Suspendisse ac sapien dolor. Duis feugiat tortor ac nisl convallis, at malesuada ligula condimentum. Nam urna enim, dictum vel libero ut, mattis semper nibh. Donec in rutrum ligula, dapibus consequat diam. Curabitur quis fringilla sapien. Nam dictum imperdiet faucibus. Ut laoreet semper gravida. Nunc sed turpis lacinia, mattis lacus eget, molestie mauris. Sed dignissim urna mi, quis ullamcorper sem porta eu. Cras pellentesque justo eget sem pretium placerat. Aenean rhoncus tristique tellus sit amet ornare. Morbi elementum nulla augue, eu luctus purus scelerisque vel. Sed viverra, odio sodales commodo mollis, quam ante convallis tortor, non ullamcorper nulla diam eget sem. Nulla eget mi nec magna viverra interdum non in massa.</p>
+
+</div>
+    <hr class="blue3">
+    
+    <!-- pbbt section 3 -->
+<div class="multipleButtons">
+<div class="multipleButtonsContent mx-auto d-block">
+    <div class="btn comboBtn">
+        <p class="font24 green1 text-left marginBottom10">Enforcement/<br>Roadside</p>
+    <div class="pic200"><a href="enforcement-roadside.php"><img src="images/button/enforcement.jpg" class="img-fluid" alt="Enforcement/Roadside"></a></div>
+<div>
+		  <a href="enforcement-roadside.php">
+                <button class="buttonLearn">Learn More</button>
+        </a> </div> 
+</div>
+    
+    <div class="btn comboBtn">
+        <p class="font24 green1 text-left marginBottom10" style="margin-top: 31px;">Fleet / Transit</p>
+    <div class="pic200"><a href="fleet-transit.php"><img src="images/button/fleet.jpg" class="img-fluid" alt="Fleet / Transit"></a></div>
+<div>
+		  <a href="fleet-transit.php">
+                <button class="buttonLearn">Learn More</button>
+        </a> </div> 
+</div>
+    
+    <div class="btn comboBtn">
+        <p class="font24 green1 text-left marginBottom10" style="margin-top: 31px;">End-of-line</p>
+    <div class="pic200"><a href="end-of-line.php"><img src="images/button/endofline.jpg" class="img-fluid" alt="End-of-line"></a></div>
+<div>
+		  <a href="end-of-line.php">
+                <button class="buttonLearn">Learn More</button>
+        </a> </div> 
+</div>
+    
+   
+    
+    </div>
+    </div>
+    </div>
+    
+    </div>
+<!-- footer -->  
+<?php include 'includes/footer.php'; ?> 
+</body>
+</html>

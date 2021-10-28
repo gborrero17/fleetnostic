7a8a3e614a363ed941d0752f8af9a9b0f5a420e4
107pilot <107pilot@gmail.com>
2021-10-28 12:35:52 -0400

    added image dropdown caption text with class subtext

--- devusol/includes/menu.php
+++ devusol/includes/menu.php
@@ -1,53 +1,54 @@
 <div id="navborder">
-   <div id="navigation-menu">
-
- 
-    
- 
-  <nav class="navbar navbar-expand-custom">
-
-    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navmenu" aria-controls="navmenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon">
-        <svg viewBox="0 0 100 80" width="35" height="40">
-          <rect width="100" height="10" rx=10></rect>
-          <rect y="30" width="100" height="10" rx=10></rect>
-          <rect y="60" width="100" height="10" rx=10></rect>
-        </svg>
-      </span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navmenu">
- 
-       
-          <ul class="navbar-nav nav-fill mr-auto w-100">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.php">ABOUT US</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a id="solutions-dropdown" img-src="images/BM3010_PP0001.jpg" href="pbbt-solutions.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">PBBT SOLUTIONS</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="air-system-expert.php">AIR SYSTEM EXPERT</a>
-            </li>
-
-            <li class="nav-item dropdown text-decoration-none" data-toggle="collapse" data-target="#navbar-collapse">
-              <a id="served-dropdown" img-src="images/PBBT_011.jpg" href="industries-served.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">INDUSTRIES SERVED</a>
-            </li>
-
-            <li class="nav-item ">
-              <a class="nav-link" href="contact.php">CONTACT US</a>
-            </li>
-          </ul>
-    
+  <div id="navigation-menu">
 
 
-    </div>
-  </nav>
-  
+
+
+    <nav class="navbar navbar-expand-custom">
+
+      <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navmenu" aria-controls="navmenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+          <svg viewBox="0 0 100 80" width="35" height="40">
+            <rect width="100" height="10" rx=10></rect>
+            <rect y="30" width="100" height="10" rx=10></rect>
+            <rect y="60" width="100" height="10" rx=10></rect>
+          </svg>
+        </span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navmenu">
+
+
+        <ul class="navbar-nav nav-fill mr-auto w-100">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">ABOUT US</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a id="solutions-dropdown" img-src="images/BM3010_PP0001.jpg" href="pbbt-solutions.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">PBBT SOLUTIONS</a>
+
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="air-system-expert.php">AIR SYSTEM EXPERT</a>
+          </li>
+
+          <li class="nav-item dropdown text-decoration-none" data-toggle="collapse" data-target="#navbar-collapse">
+            <a id="served-dropdown" img-src="images/PBBT_011.jpg" href="industries-served.php" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">INDUSTRIES SERVED</a>
+          </li>
+
+          <li class="nav-item ">
+            <a class="nav-link" href="contact.php">CONTACT US</a>
+          </li>
+        </ul>
+
+
+
+      </div>
+    </nav>
+
   </div>
 
   <div class="dropdown large-dropdown">
@@ -57,23 +58,28 @@
       <li style="margin-top: 15px" class="dropdown ">
         <hr class="line" />
         <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-14200.php">14200</a>
+        <p class="subtext d-none">sub-text 1</p>
       </li>
 
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-20200.php">20200</a>
+        <p class="subtext d-none">sub-text 2</p>
       </li>
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="pbbt-3010.php">3010</a>
+        <p class="subtext d-none">sub-text 3</p>
       </li>
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="play-detector.php">PLAY DETECTOR</a>
+        <p class="subtext d-none">sub-text 4</p>
       </li>
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="lease-audit.php">LEASE/AUDIT</a>
+        <p class="subtext d-none">sub-text 5</p>
       </li>
     </ul>
 
@@ -82,14 +88,17 @@
       <li style="margin-top: 15px" class="dropdown">
         <hr class="line" />
         <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="enforcement-roadside.php">Enforcement/Roadside</a>
+        <p class="subtext d-none">sub-text 6</p>
       </li>
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/BM3010_PP0001.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="fleet-transit.php">Fleet/Transit</a>
+        <p class="subtext d-none">sub-text 7</p>
       </li>
       <li class="dropdown">
         <hr class="line" />
         <a img-src="images/PBBT_011.jpg" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" href="end-of-line.php">End-Of-Line</a>
+        <p class="subtext d-none">sub-text 8</p>
       </li>
 
     </ul>
